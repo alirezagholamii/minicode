cc3af51fa91be5cac82e73a370ac7745f34db84d
Alireza <alireza.gholami403@gmail.com>
2021-05-04 17:43:45 +0430

    feat: add another closure example

--- js-advanced/closure.js
+++ js-advanced/closure.js
@@ -1,11 +1,11 @@
 // closures-exercise-6
-
-const array = [1,2,3,4];
-for(var i=0; i < array.length; i++) {
-  setTimeout(function(){
-    console.log('I am at index ' + i)
-  }, 3000)
-}
+//problem
+// const array = [1,2,3,4];
+// for(var i=0; i < array.length; i++) {
+//   setTimeout(function(){
+//     console.log('I am at index ' + i)
+//   }, 3000)
+// }
 
 // const array = [1,2,3,4];
 // for(let i=0; i < array.length; i++) {
@@ -15,13 +15,49 @@ for(var i=0; i < array.length; i++) {
 // }
 
 
-const array = [1,2,3,4];
-for(var i=0; i < array.length; i++) {
-  (function(closureI) {
-    setTimeout(function(){
-      console.log('I am at index ' + array[closureI])
-    }, 3000)
-  })(i)
-}     
+// const array = [1,2,3,4];
+// for(var i=0; i < array.length; i++) {
+//   (function(closureI) {
+//     setTimeout(function(){
+//       console.log('I am at index ' + array[closureI])
+//     }, 3000)
+//   })(i)
+// }     
 
+
+/***************************************/
+function a() {
+    let grandpa = 'grandpa'
+    return function b() {
+      let father = 'father'
+      return function c() {
+        let son = 'son'
+        return `${grandpa} > ${father} > ${son}`
+      }
+    }
+  }
+  
+  //a()()()
+  const one =  a();
+  const two = one();
+  const three = two()
+  console.log(three);
+  
+  //closures and higher order function
+  function boo(string) {
+    return function(name) {
+      return function(name2) {
+        console.log(`${string} ${name} ${name2}`)
+      }
+    }
+  }
+  
+  const boo2 = (string) => (name) => (name2) => console.log(`${string} ${name} ${name2}`)
+  
+  boo2('hi')('john')('tanya');
+  
+  // AHH! HOW DOES IT REMEMBER THIS 5 years from now?
+  booString = boo2('sing');
+  booStringName = booString('John');
+  booStringNameName2 = booStringName('tanya')
 
