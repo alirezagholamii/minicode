9bdf4a1aa0ed998b018a7ab779d39109de0695a9
Alireza <alireza.gholami403@gmail.com>
2021-05-05 23:23:39 +0430

    fix: closure edited

--- js-advanced/closure.js
+++ js-advanced/closure.js
@@ -1,3 +1,14 @@
+function makeFunc() {
+  var name = 'Mozilla';
+  function displayName() {
+    alert(name);
+  }
+  return displayName;
+}
+
+var myFunc = makeFunc();
+myFunc();
+
 // closures-exercise-6
 //problem
 // const array = [1,2,3,4];
@@ -27,37 +38,37 @@
 
 /***************************************/
 function a() {
-    let grandpa = 'grandpa'
-    return function b() {
-      let father = 'father'
-      return function c() {
-        let son = 'son'
-        return `${grandpa} > ${father} > ${son}`
-      }
+  let grandpa = 'grandpa'
+  return function b() {
+    let father = 'father'
+    return function c() {
+      let son = 'son'
+      return `${grandpa} > ${father} > ${son}`
     }
   }
-  
-  //a()()()
-  const one =  a();
-  const two = one();
-  const three = two()
-  console.log(three);
-  
-  //closures and higher order function
-  function boo(string) {
-    return function(name) {
-      return function(name2) {
-        console.log(`${string} ${name} ${name2}`)
-      }
+}
+
+//a()()()
+const one = a();
+const two = one();
+const three = two()
+console.log(three);
+
+//closures and higher order function
+function boo(string) {
+  return function (name) {
+    return function (name2) {
+      console.log(`${string} ${name} ${name2}`)
     }
   }
-  
-  const boo2 = (string) => (name) => (name2) => console.log(`${string} ${name} ${name2}`)
-  
-  boo2('hi')('john')('tanya');
-  
-  // AHH! HOW DOES IT REMEMBER THIS 5 years from now?
-  booString = boo2('sing');
-  booStringName = booString('John');
-  booStringNameName2 = booStringName('tanya')
+}
+
+const boo2 = (string) => (name) => (name2) => console.log(`${string} ${name} ${name2}`)
+
+boo2('hi')('john')('tanya');
+
+// AHH! HOW DOES IT REMEMBER THIS 5 years from now?
+booString = boo2('sing');
+booStringName = booString('John');
+booStringNameName2 = booStringName('tanya')
 
