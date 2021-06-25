ad4eaa8639741cb4d334a77a1ae9a02dd5090279
Alireza <alireza.gholami403@gmail.com>
2021-06-25 10:51:27 +0430

    feat: add webpack

---
+++ webpack/some-dev-tools/pizza.js
@@ -0,0 +1,9 @@
+export default {
+  pepperoni: function() {
+	  let x = 5
+    console.log('Pepperoni topping added!',x)
+  },
+  bacon: function() {
+    console.log('Bacon topping added!')
+  }
+}
---
+++ webpack/some-dev-tools/webpack.config.js
@@ -0,0 +1,33 @@
+const MiniCssExtractPlugin =  require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js%/,
+        exclude: /(node_modules)/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'main.css'
+    })
+  ],
+  watch: true
+};
