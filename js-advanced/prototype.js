81d61f3166202c690b441096fc6e267fd369b2b9
Alireza <alireza.gholami403@gmail.com>
2021-07-10 22:54:25 +0430

    feat: add prototype example

--- js-advanced/prototype.js
+++ js-advanced/prototype.js
@@ -1,4 +1,47 @@
+//1
+
+function Particle(){
+  this.x = 50;
+  this.y = 52;
+  this.show = function(){
+    console.log(this.x,this.y);
+  }
+}
+
+let p = new Particle();
+console.log(p);
+
+let p2 = new Particle();
+p2.x++;
+console.log(p2);
+
+
+// 2
+/*
+function Particle() {
+  this.x = 50;
+  this.y = 52;
+}
+
+Particle.prototype.show = function () {
+  console.log(this.x, this.y);
+}
+
+let p = new Particle();
+console.log(p);
+
+let p2 = new Particle();
+p2.x++;
+p2.show()
+console.log(p2);
+
+console.log(p2.hasOwnProperty('x')); //true
+console.log(p2.hasOwnProperty('show')); //false
+*/
+
+
 //Array.map() => to print '🗺'
+/*
 Array.prototype.map = function()  { // what happens with arrow function here?
     arr = [];
     for (let i = 0; i < this.length; i++) {
@@ -15,3 +58,6 @@ Array.prototype.map = function()  { // what happens with arrow function here?
   
   new Date('1900-10-10').lastYear()
   // don't worry if you didn't get t
+
+*/
+
