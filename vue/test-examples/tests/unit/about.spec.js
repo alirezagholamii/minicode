d0445bc8024991f4c1b2c5b850b698974e149a0d
Alireza <alireza.gholami403@gmail.com>
2021-09-03 21:39:17 +0430

    feat: add test-examples

---
+++ vue/test-examples/tests/unit/about.spec.js
@@ -0,0 +1,13 @@
+import { shallowMount } from '@vue/test-utils'
+import About from '@/views/About.vue'
+
+describe('About.vue', () => {
+  it('renders inner text', () => {
+    const wrapper = shallowMount(About);
+    expect(wrapper.text()).toContain('about')
+  })
+
+
+})
+
+
---
+++ vue/test-examples/tests/unit/hello-world.spec.js
@@ -0,0 +1,19 @@
+import { shallowMount,RouterLinkStub } from '@vue/test-utils'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+describe('HelloWorld.vue', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'new message'
+    const wrapper = shallowMount(HelloWorld, {
+      propsData: { msg },
+      global:{
+        components:{
+          'router-link': RouterLinkStub
+        }
+      }
+    })
+    expect(wrapper.text()).toContain(msg)
+  })
+})
+
+
